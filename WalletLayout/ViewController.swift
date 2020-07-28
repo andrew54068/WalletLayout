738ec008fadfa892fc218cdddf821efd5e681042
andrew54068 <andrew0424718012@yahoo.com.tw>
2020-07-28 16:24:25 +0800

    Initial Commit

---
+++ WalletLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WalletLayout
+//
+//  Created by kidnapper on 2020/7/28.
+//  Copyright © 2020 andrew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
